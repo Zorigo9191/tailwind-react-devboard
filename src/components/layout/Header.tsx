51b4987db5d60d0366536ed41a5307352cf86a03
Zorigo <zorigo.enkhtur@gmail.com>
2026-06-23 11:37:24 +0200

    fehler behoben

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,5 @@
 import { LayoutDashboard, UserCircle } from "lucide-react";
-import { Link, Links } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
